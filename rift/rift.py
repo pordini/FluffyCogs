c1bbd939d8a112df22c55d06301f1d297aff9b16
pordini <apex@imveganbtw.win>
2022-10-26 07:55:12 +0200

    Update rift.py

--- rift/rift.py
+++ rift/rift.py
@@ -179,7 +179,7 @@ class Rift(commands.Cog):
     # COMMANDS
 
     @commands.command()
-    async def send(self, ctx: commands.Context, *rifts: Messageable, *message: str):
+    async def send(self, ctx: commands.Context, *rifts: Messageable, message: str):
         """
         Send a message to the specified destinations.
 
